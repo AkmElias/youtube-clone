fc35d8be63f62673aadadf6ac4f90ee8371328ea
AkmElias <akmelias11@gmail.com>
2020-08-16 21:01:13 +0600

    Initial commit
    -- create react app using tool create-react-app
    -- clean default react landing page and test file

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhEBEE8TVECMVvXzp5oafZK0qrkV_ArME",
+  authDomain: "clone-4ed50.firebaseapp.com",
+  databaseURL: "https://clone-4ed50.firebaseio.com",
+  projectId: "clone-4ed50",
+  storageBucket: "clone-4ed50.appspot.com",
+  messagingSenderId: "1010731549355",
+  appId: "1:1010731549355:web:9355c6f65d99296a4badee",
+  measurementId: "G-KY6NPF0V04",
+};
