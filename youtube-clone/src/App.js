be93c6243fa62b509f14e81aabdc071698680fee
AkmElias <akmelias11@gmail.com>
2020-08-17 16:25:24 +0600

    SearchPage
    -- Search page UI and filtering done
    -- temporary demo design done

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -4,18 +4,35 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import RecomandedVideos from "./RecomandedVideos";
 import "./App.css";
+import SearchPage from "./SearchPage";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search/:search">
+            <div className="app__page">
+              {/* Sidebar */}
+              <Sidebar />
+              <SearchPage />
+              {/* Recomanded Video*/}
+            </div>
+          </Route>
+          <Route path="/">
+            <div className="app__page">
+              {/* Sidebar */}
+              <Sidebar />
+              <RecomandedVideos />
+              {/* Recomanded Video*/}
+            </div>
+          </Route>
+        </Switch>
+      </Router>
       {/*Header*/}
-      <Header />
-      <div className="app__page">
-        {/* Sidebar */}
-        <Sidebar />
-        <RecomandedVideos />
-        {/* Recomanded Video*/}
-      </div>
     </div>
   );
 }
--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
 
 import "./Header.css";
 
 function Header() {
+  const [inputSearch, setInputSearch] = useState("");
   return (
     <div className="header">
       <div className="header__left">
         <MenuIcon />
-        <img
-          className="header__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/9/98/YouTube_Logo.svg"
-          alt=""
-        />
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/9/98/YouTube_Logo.svg"
+            alt=""
+          />
+        </Link>
       </div>
+
       <div className="header__input">
-        <input type="text" placeholder="Search" />
-        <SearchIcon className="header__inputButton" />
+        <input
+          onChange={(e) => setInputSearch(e.target.value)}
+          value={inputSearch}
+          type="text"
+          placeholder="Search"
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
       </div>
+
       <div className="header__icons">
         <VideoCallIcon className="header__icon" />
         <AppsIcon className="header__icon" />
